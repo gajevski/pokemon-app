6a46971b9af2db75b38d69e6665dfdf257a32611
gajevski <mikolajxgajewski@gmail.com>
2020-11-24 13:48:55 +0100

    Add pokemon service

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListingComponent } from './ui-components/listing/listing.component';
 import { DetailsComponent } from './ui-components/details/details.component';
+import { PokemonService } from './pokemon.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ListingComponent, DetailsComponent]
+  declarations: [ListingComponent, DetailsComponent],
+  providers: [PokemonService]
 })
 export class PokemonModule { }
